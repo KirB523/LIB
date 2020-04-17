1f9dd375e441ca6c28563f15f723bf00cf525c63
Danis Farrakhov <doulikejazz@yandex.ru>
2020-04-17 17:03:05 +0500

    Исправлен update.bat

--- update.bat
+++ update.bat
@@ -2,6 +2,6 @@
 
 IF NOT EXIST "C:\Users\Public\Documents\Altium\Libraries\" MD "C:\Users\Public\Documents\Altium\Libraries"
 IF NOT EXIST "C:\Users\Public\Documents\Altium\Libraries\SCH" MD "C:\Users\Public\Documents\Altium\Libraries\SCH"
-COPY "G:\Work\altiumLib\LIB\SCH\*" "C:\Users\Public\Documents\Altium\Libraries\SCH\"
+COPY "SCH\*" "C:\Users\Public\Documents\Altium\Libraries\SCH\"
 COPY Data.xlsx "C:\Users\Public\Documents\Altium\Libraries\"
 COPY LIB.DbLib "C:\Users\Public\Documents\Altium\Libraries\"
