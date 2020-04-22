8bc7caec579e9143dea3aa761576cb55df0d89d6
KirB <badlight23@gmail.com>
2020-04-22 18:01:28 +0500

    Обновлены для BuckQR

--- update.bat
+++ update.bat
@@ -2,6 +2,8 @@
 
 IF NOT EXIST "C:\Users\Public\Documents\Altium\Libraries\" MD "C:\Users\Public\Documents\Altium\Libraries"
 IF NOT EXIST "C:\Users\Public\Documents\Altium\Libraries\SCH" MD "C:\Users\Public\Documents\Altium\Libraries\SCH"
+IF NOT EXIST "C:\Users\Public\Documents\Altium\Libraries\PCB" MD "C:\Users\Public\Documents\Altium\Libraries\PCB"
+COPY "PCB\*" "C:\Users\Public\Documents\Altium\Libraries\PCB\"
 COPY "SCH\*" "C:\Users\Public\Documents\Altium\Libraries\SCH\"
 COPY Data.xlsx "C:\Users\Public\Documents\Altium\Libraries\"
 COPY LIB.DbLib "C:\Users\Public\Documents\Altium\Libraries\"
